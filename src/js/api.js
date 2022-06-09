d2dd18db6b0394abbc78124b8abf0a6172eaa9a7
Nefalit <nefalit7@gmail.com>
2022-06-09 20:22:35 +0300

    add api

--- src/js/api.js
+++ src/js/api.js
@@ -1,2 +1,33 @@
-ROOT_API_KEY: "f3ea85ad66a7076fbd3968a20cd79e45"
-BASE_URL:"https://api.themoviedb.org/3/search/movie"
+import axios from 'axios';
+
+// ROOT_API_KEY: 'f3ea85ad66a7076fbd3968a20cd79e45';
+// BASE_URL: 'https://api.themoviedb.org/3/search/movie';
+
+export default class FilmsApi {
+  #ROOT_API_KEY = 'api_key=f3ea85ad66a7076fbd3968a20cd79e45';
+  #BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+
+  constructor() {
+    this.query = null;
+    this.page = 1;
+    this.lang = 'uk';
+  }
+  
+// при вызове методов в первом then делаем деструктаризацию {data}
+
+  searchPopularFilms() {
+    return axios.get(
+      `${this.#BASE_URL}/week?${this.#ROOT_API_KEY}&page=${
+        this.page
+      }&language=${this.lang}`
+    );
+  }
+
+  searchFilmsByQuery(query, page) {
+    return axios.get(
+      `${this.#BASE_URL}/?${
+        this.#ROOT_API_KEY
+      }&query=${query}&page=${page}&language=${this.lang}`
+    );
+  }
+}
