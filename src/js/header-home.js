e32d36a587aea45f10c9091185b9d1e1e692dc1e
KORZH ILLIA <kiev_drum2006@ukr.net>
2022-06-10 22:59:17 +0300

    General changes

--- src/js/header-home.js
+++ src/js/header-home.js
@@ -1,12 +1,26 @@
-import FilmsApi from './api';
+import filmCard from '../templates/film-card.hbs';
+import { getYear, getPosterUrl } from './handlebars.js';
+import { filmsApi } from './gallery-home';
+import { galleryEl } from './gallery-home';
+import { CURRENT_PAGE_FILMS } from '..';
 
-const searchForm = document.querySelector('js-search__input');
-const searchBtn = document.querySelector('js-search__btn');
+export const searchForm = document.querySelector('.js-search-form');
+export const searchBtn = document.querySelector('.js-search-btn');
+export const searchWarn = document.querySelector('.js-search-warn');
 
+searchWarn.classList.add('is-hidden');
 searchForm.addEventListener('submit', searchFormSubmitHandler);
 
-function searchFormSubmitHandler(event) {
+export async function searchFormSubmitHandler(event) {
+  event.preventDefault();
   const { searchQuery } = event.currentTarget.elements;
-  const query = searchquery.value;
-  return;
+  const query = searchQuery.value;
+  try {
+    const moviesArray = await filmsApi.searchFilmsByQuery(query);
+    galleryEl.innerHTML = filmCard(moviesArray);
+    localStorage.setItem(CURRENT_PAGE_FILMS, JSON.stringify(moviesArray));
+  } catch (error) {
+    console.log(error);
+    searchWarn.classList.remove('is-hidden');
+  }
 }
