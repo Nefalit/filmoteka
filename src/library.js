9663176f405d8595fdab8122a024d39ba4a40c3a
KORZH ILLIA <kiev_drum2006@ukr.net>
2022-06-11 19:42:45 +0300

    Modal finally

--- src/library.js
+++ src/library.js
@@ -1,5 +1,5 @@
-import header from "./js/header-library.js"
-
+import header from './js/header-library.js';
+import gallery from './js/gallery-library.js';
 export const CURRENT_PAGE_FILMS = 'currentPageFilms';
 export const WATCHED_PAGE_FILMS = 'watchedPageFilms';
 export const QUEUE_PAGE_FILMS = 'queuePageFilms';
