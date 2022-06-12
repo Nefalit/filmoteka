396fcf7792feec785d0cea571d808fe7fadb7966
Liza Chernysheva <lizzychernysheva@gmail.com>
2022-06-13 00:13:52 +0300

    pagination without styles

--- src/library.js
+++ src/library.js
@@ -1,5 +1,7 @@
 import header from './js/header-library.js';
 import gallery from './js/gallery-library.js';
+import { pagination } from './js/gallery-library-pagination';
+
 export const CURRENT_PAGE_FILMS = 'currentPageFilms';
 export const WATCHED_PAGE_FILMS = 'watchedPageFilms';
 export const QUEUE_PAGE_FILMS = 'queuePageFilms';
