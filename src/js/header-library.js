593f668b8f526632ac8374a100192a1050a344ab
KORZH ILLIA <kiev_drum2006@ukr.net>
2022-06-13 00:06:14 +0300

    Pagination semi-works, minor CSS fixings

--- src/js/header-library.js
+++ src/js/header-library.js
@@ -10,15 +10,25 @@ export const galleryLibEl = document.querySelector('.gallery-library__list');
 export const watchedArr = load('watchedPageFilms');
 export const queueArr = load('queuePageFilms');
 
+btnQueueEl.classList.toggle('btn--active');
+
 btnWatchedEl.addEventListener('click', getDataWatched);
 btnQueueEl.addEventListener('click', getDataQueue);
 
 function getDataWatched() {
+  btnWatchedEl.classList.add('btn--active');
+  btnWatchedEl.classList.remove('btn--bright');
+  btnQueueEl.classList.add('btn--bright');
+  btnQueueEl.classList.toggle('btn--active');
   const watchedMarkup = libraryFilmCard(watchedArr);
   galleryLibEl.innerHTML = watchedMarkup;
 }
 
 export function getDataQueue() {
+  btnQueueEl.classList.add('btn--active');
+  btnQueueEl.classList.remove('btn--bright');
+  btnWatchedEl.classList.add('btn--bright');
+  btnWatchedEl.classList.toggle('btn--active');
   const queueMarkup = libraryFilmCard(queueArr);
   galleryLibEl.innerHTML = queueMarkup;
 }
